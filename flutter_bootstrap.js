dead2d48ef387c617bc895af1265b4f4ef957845
urGirlkiri <urGirlkiri@users.noreply.github.com>
2026-05-12 20:36:48 +0000

    deploy: b51b570eca28b933b0e329220e230c78b3f025c0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2426574928" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2070362045" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
